745b61731f7e3cc21e69427f5f060878ab56d246
mocha <mocha4pf@gmail.com>
2026-04-28 10:41:07 +0300

    added formula

--- code.js
+++ code.js
@@ -24,7 +24,11 @@ const maleNames = [
 
 const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
+//use the data and place them in relevant variables for the formula
 let CC = Math.floor(year / 100);
 let YY = year % 100;
 let MM = month;
 let DD = day;
+
+//use the extracted data in the formula
+let d = (CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7;
