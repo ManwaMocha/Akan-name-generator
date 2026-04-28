45aace6db89cb9a8d149208c886a0a34a2236491
mocha <mocha4pf@gmail.com>
2026-04-28 10:33:23 +0300

    added table data on names to arrays

--- code.js
+++ code.js
@@ -2,4 +2,24 @@
 const form = document.getElementById("akanForm");
 const result = document.getElementById("result");
 
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
+const maleNames = [
+  "Kwasi",
+  "Kwadwo",
+  "Kwabena",
+  "Kwaku",
+  "Yaw",
+  "Kofi",
+  "Kwame",
+];
+
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
